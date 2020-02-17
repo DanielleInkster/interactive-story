6250ef7b603f8d1ced1ea4d75f7d261be7be168c
DanielleInkster <danielle.inkster@gmail.com>
2020-02-17 23:58:09 +0000

    more story stuff

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ include "turn.php";
 echo "Welcome stranger! What is your name?\n";
 $name = readline("\n >> ");
 echo "\nWelcome {$name}!\n";
-echo "\nYou're just in time! The village of Thirnov is in great peril \nand needs a hero such as yourself to save them. \nWill you accept the quest?\n";
+echo "\nYou're just in time! The village of Thirnov is in great \nqperil and needs a hero such as yourself to save them. \nWill you accept the quest?\n";
 $answer = readline("\n >> ");
 while (strtolower($answer) != "yes"){ echo "Please reconsider. Will you accept the quest?\n"; $answer = readline("\n >> ");}
-echo "\n Fantastic! Let's begin...";
+echo "\n Fantastic! Let's begin...\n";
+echo "***\n The villager leads you to the mouth of a cave.\n He hands you a sword and shoves you towards the opening.\n***";
+echo "\nIt's in there, {$name}...";
+
 take_turn();
 ;

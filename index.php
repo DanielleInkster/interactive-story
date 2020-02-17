6b833a7ce045bb2de82fa01f541484931e9398c0
DanielleInkster <danielle.inkster@gmail.com>
2020-02-17 23:10:42 +0000

    Start game

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 echo "Welcome stranger! What is your name?\n";
-$name = readline("\n >>");
-
-echo "Welcome {$name}!"
-
+$name = readline("\n >> ");
+echo "\nWelcome {$name}!\n";
+echo "\nYou're just in time! The village of Thirnov is in great peril \nand needs a hero such as yourself to save them. \nWill you accept the quest?\n";
+$answer = readline("\n >> ");
+do { echo "Please reconsider. Will you accept the quest?\n"; $answer = readline("\n >> ");} while (strtolower($answer) != "yes");
+echo "\n Fantastic! Let's begin...";
 
 ;

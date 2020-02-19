8fa9562b3029b0fab8751a49bf1f281a705bd635
DanielleInkster <danielle.inkster@gmail.com>
2020-02-19 18:31:52 +0000

    added run_action

--- action.php
+++ action.php
@@ -2,7 +2,23 @@
 
 function choose_action($action){
     if(strtolower($action)=== 'speak'){
-        echo "DO THE THING";
+        echo "'What do you want?' you ask.";
+    } elseif(strtolower($action)=== 'run'){
+        echo run_action();
     }
-}
+};
+
+function speak_action(){
+
+};
+
+function fight_action(){
+    
+};
+
+function run_action(){
+    echo "You turn to run back to the opening of the cave. \nThe dragon burns you to a crisp. \n";
+    echo "Better luck next time!";
+    exit();
+};
 ;

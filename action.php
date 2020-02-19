db876e1e1543f2d06cf7bec4f86a2d600a0e92df
DanielleInkster <danielle.inkster@gmail.com>
2020-02-19 18:16:21 +0000

    turns increment up to 3

---
+++ action.php
@@ -0,0 +1,6 @@
+<?php
+
+function choose_action($action){
+    
+}
+;
--- turn.php
+++ turn.php
@@ -1,8 +1,14 @@
 <?php
 
+include "action.php";
+$num_of_turns = 0;
+
 function take_turn(){
-   echo "*** \nWhat will you do? \n***";
-}
-
-
-;
+   while ($num_of_turns < 3){
+   echo "\n*** \nWhat will you do? \n***";
+   $action = readline("\n >> ");
+  echo "{$action}";
+   $num_of_turns++;
+   echo "{$num_of_turns}";
+   }
+};

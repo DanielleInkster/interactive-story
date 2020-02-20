0de7a4a60b1ec859a725c7449b4b24dc6f11ae07
DanielleInkster <danielle.inkster@gmail.com>
2020-02-20 16:05:21 +0000

    finished game

--- action.php
+++ action.php
@@ -1,18 +1,35 @@
 <?php
+$num_of_tries = 0;
 
 function choose_action($action){
     if(strtolower($action)=== 'speak'){
-        echo "'What do you want?' you ask.";
+        echo speak_action();
     } elseif(strtolower($action)=== 'run'){
         echo run_action();
     } elseif(strtolower($action)=== 'fight'){
         echo fight_action();
     } else {
-        echo "\n***\nInvalid selection. \nChoices are 'run', 'fight' or 'speak'.\n***";
+        echo "\n***\nInvalid selection. \nChoices are 'run', 'fight' or 'speak'.\n***\n";
+        $num_of_turns-=1;
     }
 };
 
 function speak_action(){
+    echo "\n'What do you want?' you ask.";
+    echo "\n Answer my riddle and I'll spare your life' the dragon replies. \n'What's orange and sounds like a parrot?' ";
+    $response = readline("\n >> ");
+    while (strtolower($response) != "a carrot" && $num_of_turns < 3){ echo "\nWRONG! What's orange and sounds like a parrot?"; 
+        $response = readline("\n >> ");
+        $num_of_tries++;
+        if($num_of_tries === 2){
+            echo "\nThe dragon grows impatient and swallows you whole. \n\n***GAME OVER***";
+            exit();
+        }
+    }
+    echo "\n'Well done!' cries the dragon. \n'I should spare your life...but I'm a hungry dragon and kind of a jerk.'\n";
+    echo "\nThe dragon swallows you whole. \nThat's what you get for trusting a dragon, I guess.\n";
+    echo "\n\n ***GAME OVER***\n\n";
+    exit();
 
 };
 

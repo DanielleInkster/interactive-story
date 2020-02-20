563108c2f726df3f185b9dc90c1c359e91e6005e
DanielleInkster <danielle.inkster@gmail.com>
2020-02-20 16:59:56 +0000

    Read me

---
+++ README.md
@@ -0,0 +1,30 @@
+# INTERACTIVE STORY GAME #
+
+### Description ###
+
+A simple single-player story game written in PHP to be played in the terminal. Player is given a simple quest to defeat a dragon using set moves. 
+
+### Technologies ###
+PHP 7.3.11
+
+### To install and run ###
+```
+> git clone git@github.com:DanielleInkster/interactive-story.git
+> navigate to src directory
+> php index.php
+```
+
+### Reflections ###
+
+- I created this project to practice applying the basics of PHP. It uses 
+
+### Next steps ###
+- Testing
+- Possible incorporation of HTML to play on a webpage.
+
+
+
+
+
+
+
--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@ include "turn.php";
 echo "Welcome stranger! What is your name?\n";
 $name = readline("\n >> ");
 echo "\nWelcome {$name}!\n";
-echo "\nYou're just in time! The village of Thirnov is in great \nperil and needs a hero such as yourself to save them. \n\nWill you accept the quest?\n";
+echo "\nYou're just in time! The village of Thirnov is in great \nperil and needs a hero such as yourself to save them from a terrible dragon."; 
+echo "\n\nWill you accept the quest?\n";
 $answer = readline("\n >> ");
-
-
+while (strtolower($answer) != "yes"){ echo "Please reconsider. Will you accept the quest?\n"; $answer = readline("\n >> ");}
 echo "\n Fantastic! Let's begin...\n\n";
 echo "***\n\n You are brought to the mouth of a cave.\n You are handed a sword and shoved towards the opening. ";
 echo "\n You enter the cave to see a fierce dragon!\n\n***";
